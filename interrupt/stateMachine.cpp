60dd3ae570c14f6062de2c709820a68298170603
Alex M <57399961+IroniX2@users.noreply.github.com>
2021-05-27 00:00:18 +0200

    add necessary methods to state machine and fix imports

--- interrupt/stateMachine.cpp
+++ interrupt/stateMachine.cpp
@@ -1,2 +1,32 @@
 #include "stateMachine.h"
+/*
+unsigned char state;
 
+// unsigned int trackSensorAddresses[14][2] = {{13, 0}, {12, 0}, {11, 0}, {10, 0}, {9, 0}, {8, 0}, {7, 0}, {6, 0}, {5, 0}, {A0, 0}, {A1, 0}, {A2, 0}, {A3, 0}, {A4, 0}};
+
+void clearSensorCounters(unsigned int **trackSensorAddresses)
+{
+    for (unsigned short i = 0; i < (sizeof(trackSensorAddresses) / sizeof(trackSensorAddresses[0])); i++)
+    {
+        trackSensorAddresses[i][1] = 0;
+    }
+}
+
+void readSensorCounters(unsigned int **trackSensorAddresses)
+{
+    for (short i = 0; i < (sizeof(trackSensorAddresses) / sizeof(trackSensorAddresses[0])); i++)
+    {
+        Serial.print(trackSensorAddresses[i][0]);
+        Serial.print(" - ");
+        Serial.println(trackSensorAddresses[i][1]);
+    }
+}
+
+void stateMachine()
+{
+    switch(state)
+    {
+        
+    }
+}
+*/
--- interrupt/stateMachine.h
+++ interrupt/stateMachine.h
@@ -1,6 +1,6 @@
 #if !defined(TRAIN_CODE_C_STATE_MACHINE)
 #define TRAIN_CODE_C_STATE_MACHINE
 
-
+#include <Arduino.h>
 
 #endif // TRAIN_CODE_C_STATE_MACHINE
